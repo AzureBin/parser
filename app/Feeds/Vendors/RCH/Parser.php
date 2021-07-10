08a50b2d8bd7d8075da7fb24b351110c1a4fa6fd
Raja Omer Mustafa <o.mustafa.pseb@gmail.com>
2021-07-10 21:12:39 +0500

    fetch item using regrex

--- app/Feeds/Vendors/RCH/Parser.php
+++ app/Feeds/Vendors/RCH/Parser.php
@@ -19,58 +19,60 @@ class Parser extends HtmlParser
     private string $mpn = '';
     private string $product = '';
     private ?string $upc = null;
-    private array $desc = [];
+    private string $desc = '';
 
     public function beforeParse(): void
     {
         $this->filter( '#tab-description ul li' )->each( function ( ParserCrawler $c ) {
             if ( str_contains( $c->text(), 'Weight:' ) ) {
                 if (preg_match('#\((.*?)\)#', $c->text(), $m)) {
-                    // dd($m);
                     $this->shipping_weight = (float) preg_replace("/[^0-9.]/", "", $m[1]);
                 }
-                // $this->shipping_weight = str_replace("Weight:", "", $c->text());
             }
             elseif ( str_contains( $c->text(), 'Length:' ) ) {
                 if (preg_match('#\((.*?)\)#', $c->text(), $m)) {
-                    // dd($m);
                     $this->dims[ 'x' ] = (float) preg_replace("/[^0-9.]/", "", $m[1]);
                 }
-                // $this->dims[ 'x' ] = str_replace("Length:", "", $c->text());
             }
             elseif ( str_contains( $c->text(), 'Width:' ) ) {
                 if (preg_match('#\((.*?)\)#', $c->text(), $m)) {
-                    // dd($m);
                     $this->dims[ 'y' ] = (float) preg_replace("/[^0-9.]/", "", $m[1]);
                 }
             }
         });
 
-        $this->desc = $this->filter( '#tab-description p' )->each( function ( ParserCrawler $c ) {
-            return $c->filter( 'strong' )->each( function ( ParserCrawler $d ) {
-                if ( str_contains( $d->text(), 'FEATURES:' ) ) {
-                    $features = $d->parents()->nextAll();
-                    $features->filter( 'ul li' )->each( function ( ParserCrawler $e ) {
-                        array_push($this->shorts, $e->text());
-                    });
-                    foreach ($features as $node) {
-                        $node->parentNode->removeChild($node);
-                    }
-                }
-                if ( str_contains( $d->text(), 'TECHNICAL SPECS:' ) ) {
-                    $technicalSpecs = $d->parents()->nextAll();
-                    $technicalSpecs->filter( 'ul li' )->each( function ( ParserCrawler $e ) {
-                        if ( str_contains( $e->text(), ':' ) ) {
-                            [ $key, $val ] = explode( ':', $e->text() );
-                            $this->attrs[$key] = $val;
-                        }
-                    });
-                    foreach ($technicalSpecs as $node) {
-                        $node->parentNode->removeChild($node);
-                    }
+        $description = $this->getHtml('#tab-description');
+        $description = preg_replace("/\r|\n/", "", $description);
+
+        $technicalSpecsPattern = "/TECHNICAL SPECS:.*?<\/p><ul>(.*?)<\/ul>/mui";
+        preg_match_all($technicalSpecsPattern, $description, $result);
+        $attrs = [];
+        if (isset($result[1][0])) {
+            $crawler = new ParserCrawler($result[1][0]);
+            $crawler->filter( 'li' )->each( function ( ParserCrawler $c ) use (&$attrs) {
+                if ( str_contains( $c->text(), ':' ) ) {
+                    [ $key, $val ] = explode( ':', $c->text() );
+                    $attrs[ StringHelper::normalizeSpaceInString( $key ) ] = StringHelper::normalizeSpaceInString( $val );
                 }
             });
-        });
+        }
+        $this->attrs = $attrs;
+        $featurePattern = "/<p.*?>Features:.*?<\/p><ul.*?>(.*?)<\/ul>/mui";
+        preg_match_all($featurePattern, $description, $featureResult);
+        $shorts = [];
+        if (isset($featureResult[1][0])) {
+            $crawler = new ParserCrawler($featureResult[1][0]);
+            $crawler->filter( 'li' )->each( function ( ParserCrawler $c ) use (&$shorts) {
+                $shorts[] = $c->text();
+            });
+        }
+        $this->shorts = $shorts;
+
+        $descriptionPattern = "/<p>.*?(?=Features:)/mui";
+        preg_match_all($descriptionPattern, $description, $descriptionResult);
+        if (isset($descriptionResult[0][0])) {
+            $this->desc = $descriptionResult[0][0];
+        }
 
         $this->filter( 'meta' )->each( function ( ParserCrawler $c ) {
             if ($c->attr('itemprop') == 'mpn')
@@ -112,17 +114,12 @@ class Parser extends HtmlParser
 
     public function getDescription(): string
     {
-        // return trim($this->desc);
-        return trim($this->getText('#tab-description'));
+        return trim($this->desc);
     }
 
     public function getShortDescription(): array
     {
-        return $this->shorts;
-        if ( $this->exists( '#tab-description p' ) ) {
-            return $this->getContent( '#tab-description p' );
-        }
-        return [];
+        return $this->shorts ?? [];
     }
 
     public function getAttributes(): ?array
